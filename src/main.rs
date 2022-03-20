b89a6ff0b308cda2e7d7864661c2ed13e6122407
Brandon <highb@users.noreply.github.com>
2022-03-19 20:54:43 -0700

    Refactor server address to be configurable at runtime

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    run("127.0.0.1:8000")?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -30,7 +30,6 @@ async fn health_check_works() {
 // if we fail to perform the required setup we can just panic and crash
 // all the things.
 fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
